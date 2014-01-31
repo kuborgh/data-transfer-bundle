a19a4982a852030c8da4b55c847446e4f66bb8b1
Hannes Giesenow <hgiesenow@kuborgh.de>
2014-01-31 16:33:13 +0100

    Implemented file transfer

--- Command/FetchCommand.php
+++ Command/FetchCommand.php
@@ -11,6 +11,7 @@
 namespace Kuborgh\DataTransferBundle\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
@@ -20,13 +21,32 @@ use Symfony\Component\Process\Process;
 class FetchCommand extends AbstractCommand
 {
     /**
+     * Regex to check if the remote dump is ok
+     * must start with '-- MySQL dump'
+     */
+    const VALID_DUMP_REGEX_1 = '/^\-\- MySQL dump/';
+
+    /**
+     * Regex to check if the remote dump is ok
+     * must end with '-- Dump completed'
+     */
+    const VALID_DUMP_REGEX_2 = '/\-\- Dump completed on \d*\-\d*\-\d* \d+\:\d+\:\d+[\r\n\s\t]*$/';
+
+    /**
      * Configure command
      */
     protected function configure()
     {
         $this
             ->setName('data-transfer:fetch')
-            ->setDescription('Fetch remote database and files from configured system.');
+            ->setDescription('Fetch remote database and files from configured system.')
+            ->addOption('db-only', 'db-only', InputOption::VALUE_NONE, 'Only transfer the database, not the files.')
+            ->addOption(
+                'files-only',
+                'files-only',
+                InputOption::VALUE_NONE,
+                'Only transfer the files, not the database.'
+            );
     }
 
     /**
@@ -42,15 +62,25 @@ class FetchCommand extends AbstractCommand
         parent::execute($input, $output);
 
         // Fetch and import live database
-        try {
-            $this->fetchDatabase();
-        } catch (\Exception $exc) {
-            $this->progressErr($exc->getMessage());
+        if (!$input->getOption('files-only')) {
+            try {
+                $this->fetchDatabase();
+            } catch (\Exception $exc) {
+                $this->progressErr($exc->getMessage());
+            }
+            $this->progressDone();
         }
-        $this->progressDone();
 
         // fetch live data files
-        $this->fetchFiles();
+        if (!$input->getOption('db-only')) {
+            try {
+                $this->fetchFiles();
+            } catch (\Exception $exc) {
+                $this->progressErr($exc->getMessage());
+            }
+            $this->progressDone();
+        }
+
     }
 
     /**
@@ -59,21 +89,21 @@ class FetchCommand extends AbstractCommand
     protected function fetchDatabase()
     {
         $this->output->writeln('Fetching database');
-        // Get and test database credentials
-        // @todo
 
         // Prepare remote command
         $remoteHost = $this->getParam('remote.host');
         $remoteUser = $this->getParam('remote.user');
         $remoteDir = $this->getParam('remote.dir');
         $remoteEnv = $this->getParam('remote.env');
+        $consoleCmd = $this->getParam('console_script');
         $options = $this->getParam('ssh.options');
         $exportCmd = sprintf(
-            'ssh %s %s@%s php %s/console %s data-transfer:export 2>&1',
+            'ssh %s %s@%s "cd %s ; %s %s data-transfer:export 2>&1"',
             implode(' ', $options),
             $remoteUser,
             $remoteHost,
             $remoteDir,
+            $consoleCmd,
             $remoteEnv ? '--env=' . $remoteEnv : ''
         );
         $this->progress();
@@ -82,33 +112,22 @@ class FetchCommand extends AbstractCommand
         $process = new Process($exportCmd);
         $process->run();
         $this->progress();
-
         // Check for error
         if (!$process->isSuccessful()) {
             throw new \Exception(sprintf('Cannot connect to remote host: %s', $process->getOutput()));
         }
 
         // Check if we have a valid dump in our output
-        $sqlDump = $process->getOutput();
-
         // first line must start with '-- MySQL dump' and end with '-- Dump completed'
-        if (!preg_match('/^\-\- MySQL dump/', $sqlDump) || !preg_match(
-                '/\-\- Dump completed on \d*\-\d*\-\d* \d+\:\d+\:\d+[\r\n\s\t]*$/',
-                $sqlDump
-            )
-        ) {
+        $sqlDump = $process->getOutput();
+        if (!preg_match(self::VALID_DUMP_REGEX_1, $sqlDump) || !preg_match(self::VALID_DUMP_REGEX_2, $sqlDump)) {
             throw new \Exception(sprintf('Error on remote host: %s', $process->getOutput()));
         }
 
-        // Otherwise we now have sql dump in our output
-        $this->progressOk();
-
-        // Save to temporary file
+        // Save dump to temporary file
         $tmpFile = $this->getContainer()->getParameter('kernel.cache_dir') . '/data-transfer.sql';
         file_put_contents($tmpFile, $sqlDump);
         $this->progress();
-
-        // Import Dump
 
         // Fetch db connection data
         $siteaccess = $this->getContainer()->getParameter('data_transfer_bundle.siteaccess');
@@ -118,8 +137,7 @@ class FetchCommand extends AbstractCommand
         $dbPass = $dbParams['password'];
         $dbHost = $dbParams['host'];
 
-//        die(escapeshellarg($sqlDump));
-
+        // Import Dump
         $importCmd = sprintf(
             'mysql %s --user=%s --password=%s --host=%s < %s 2>&1',
             escapeshellarg($dbName),
@@ -135,6 +153,12 @@ class FetchCommand extends AbstractCommand
             throw new \Exception(sprintf('Error importing database : %s', $process->getOutput()));
         }
         $this->progressOk();
+
+        // Remove temp dump file
+        if (file_exists($tmpFile)) {
+            unlink($tmpFile);
+        }
+        $this->progress();
     }
 
     /**
@@ -143,11 +167,44 @@ class FetchCommand extends AbstractCommand
     protected function fetchFiles()
     {
         $this->output->writeln('Fetching files');
-        // Use rsync to fetch files
-        // @todo
-        $this->progressErr('Not implemented yet');
 
-        $this->progressDone();
+        // Fetch folders to rsync
+        $folders = $this->getParam('folders');
+        $rsyncOptions = $this->getParam('rsync.options');
+        $sshOptions = $this->getParam('ssh.options');
+
+        // Fetch params
+        $remoteHost = $this->getParam('remote.host');
+        $remoteUser = $this->getParam('remote.user');
+        $remoteDir = $this->getParam('remote.dir');
+
+        // Loop over the folders, to be transfered
+        foreach ($folders as $folder) {
+            // Prepare command
+            $cmd = sprintf(
+                'rsync %s -e "ssh %s" %s@%s:%s/%s %s/ 2>&1',
+                implode(' ', $rsyncOptions),
+                implode(' ', $sshOptions),
+                $remoteUser,
+                $remoteHost,
+                $remoteDir,
+                $folder,
+                dirname($folder)
+            );
+
+            // Run (with callback to update those fancy dots
+            $process = new Process($cmd);
+            $process->run(
+                function () {
+                    $this->progress();
+                }
+            );
+            if (!$process->isSuccessful()) {
+                throw new \Exception(sprintf('Error fetching files: %s', $process->getOutput()));
+            }
+
+            $this->progressOk();
+        }
     }
 
     /**
