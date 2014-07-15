49f3ce0faee872bde9b1c39bcf044d8032051cc2
kuborgh-mspindelhirn <mspindelhirn@kuborgh.de>
2014-07-15 17:40:04 +0200

    added backwards compatibility for database settings

--- Command/FetchCommand.php
+++ Command/FetchCommand.php
@@ -158,16 +158,31 @@ class FetchCommand extends AbstractCommand
 
         // Fetch db connection data
         $siteaccess = $this->getContainer()->getParameter('data_transfer_bundle.siteaccess');
-        $repository = $this->getContainer()->getParameter(sprintf('ezsettings.%s.repository', $siteaccess));
-        $repositories = $this->getContainer()->getParameter('ezpublish.repositories');
-        $connection = $repositories[$repository]['connection'];
-        /** @var $dbalConnection Connection */
-        $dbalConnection = $this->getContainer()->get(sprintf('doctrine.dbal.%s_connection', $connection));
 
-        $dbName = $dbalConnection->getDatabase();
-        $dbUser = $dbalConnection->getUsername();
-        $dbPass = $dbalConnection->getPassword();
-        $dbHost = $dbalConnection->getHost();
+        $legacyParameter = sprintf('ezsettings.%s.database.params', $siteaccess);
+        $repositoryParameter = sprintf('ezsettings.%s.repository', $siteaccess);
+		
+        if($this->getContainer()->hasParameter($legacyParameter)) {
+            $dbParams = $this->getContainer()->getParameter($legacyParameter);
+            $dbName = $dbParams['database'];
+            $dbUser = $dbParams['user'];
+            $dbPass = $dbParams['password'];
+            $dbHost = $dbParams['host'];
+        } elseif ($this->getContainer()->hasParameter($repositoryParameter)) {
+            $repository = $this->getContainer()->getParameter($repositoryParameter);
+            $repositories = $this->getContainer()->getParameter('ezpublish.repositories');
+            $connection = $repositories[$repository]['connection'];
+            /** @var $dbalConnection Connection */
+            $dbalConnection = $this->getContainer()->get(sprintf('doctrine.dbal.%s_connection', $connection));
+
+            $dbName = $dbalConnection->getDatabase();
+            $dbUser = $dbalConnection->getUsername();
+            $dbPass = $dbalConnection->getPassword();
+            $dbHost = $dbalConnection->getHost();
+        } else {
+            $message = "Unable to find database settings from siteaccess. You need to define either %s or %s";
+            throw new \Exception(sprintf($message, $legacyParameter, $repositoryParameter));
+        }
 
         // Import Dump
         $importCmd = sprintf(
